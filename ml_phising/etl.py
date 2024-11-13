186d9ec68e0aaec4479ea177feca28186dfb2013
Manan Doshi <manan.doshi@colorado.edu>
2024-11-12 22:29:20 -0700

    NEw file added to preprocess

--- ml_phising/etl.py
+++ ml_phising/etl.py
@@ -7,17 +7,17 @@ from collections import Counter
 from urllib.parse import urlparse
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 
-def label_encoding(label):    
+def label_encoding(label):
     # Encode Module
     label_encoder = LabelEncoder()
-    df['label_encoded'] = label_encoder.fit_transform(df[label])    
+    df['label_encoded'] = label_encoder.fit_transform(df[label])
 
 def feature_extraction(url):
     # Feature Extraction
     # Adding url length
     df['url_length'] = df[url].apply(len)
 
-    # Special Character Count    
+    # Special Character Count
     df['num_special_chars'] = df[url].apply(lambda x: sum([1 for char in x if char in '-_?=&']))
 
 def normalization(url_length = 'url_length', num_special_chars = 'num_special_chars') -> None:
@@ -32,16 +32,16 @@ def no_of_subdomains(url = 'urls'):
         try:
             parser = urlparse(string)
             total = max(parser.netloc.count('.') - 1, 0)
-        except Exception as e: pass            
+        except Exception as e: pass
         return total
-    
-    df["num_subdomains"] = df[url].apply(get_total_subdomains)    
+
+    df["num_subdomains"] = df[url].apply(get_total_subdomains)
     # print("Total Unique Subdomains:", df["num_subdomains"].unique())
 
 def is_https(url = 'url'):
     def identify_if_https(string):
         return 1 if string.startswith('https') else 0
-        
+
     df["is_https"] = df[url].apply(identify_if_https)
 
 def is_domain_ip(url = 'url'):
@@ -56,7 +56,7 @@ def is_domain_ip(url = 'url'):
 def has_suspicious_keywords(url = 'url'):
     def suspicious_keywords():
         sus_keywords = ['login', 'verify', 'account', 'update', 'secure']
-        
+
         ...
     df['total_suspicious_keywords'] = df[url].apply(suspicious_keywords)
 
@@ -81,28 +81,31 @@ def entropy(url = 'url'):
         total = 0
         for c in string:
             prob = float(d[c]) / n
-            total += prob * np.log2(prob)        
+            total += prob * np.log2(prob)
         return -total
 
-    df['url_entropy'] = df[url].apply(calculate_entropy)    
+    df['url_entropy'] = df[url].apply(calculate_entropy)
 
 def store_to_preprocess_csv(raw_data_filename, index = False) -> None:
     filename = raw_data_filename.split('.')[0]
-    df.to_csv(f"../data/preprocess/{filename}_preprocess.csv", index = False)    
+    df.to_csv(f"../data/preprocess/{filename}_preprocess.csv", index = False)
 
 
 # Dataset details
 dataset = {
-    "1": {"filename": "kaggle_balanced_urls.csv", "label_encode_on": "label", "feature_extraction_on": "url"},    
-    "2": {"filename": "kaggle_malicious_phish.csv", "label_encode_on": "type", "feature_extraction_on": "url"}, 
-    "3": {"filename": "PhiUSIIL_Phishing_URL_Dataset_uci_2024.csv", "label_encode_on": "label", "feature_extraction_on": "url"},     
+    "1": {"filename": "kaggle_balanced_urls.csv", "label_encode_on": "label", "feature_extraction_on": "url"},
+    "2": {"filename": "kaggle_malicious_phish.csv", "label_encode_on": "type", "feature_extraction_on": "url"},
+    "3": {"filename": "PhiUSIIL_Phishing_URL_Dataset_uci_2024.csv", "label_encode_on": "label", "feature_extraction_on": "url"},
+    "4": {"filename": "all_url.csv", "excess_path":"Phishing_URL_dataset/","label_encode_on": "type", "feature_extraction_on": "url"},
 }
 
-dataset_details = dataset["2"]
+dataset_details = dataset["4"]
 
 # Filename
 raw_data_filename = dataset_details["filename"]
-raw_data_filepath = '../data'
+raw_data_filepath = '../data/csvs/'
+if dataset_details["excess_path"]:
+    raw_data_filepath = os.path.join(raw_data_filepath, dataset_details["excess_path"])
 filepath = os.path.join(raw_data_filepath, raw_data_filename)
 
 # Reading dataset
@@ -112,7 +115,7 @@ parser = argparse.ArgumentParser(description="ETL script for preprocessing")
 parser.add_argument("--preprocess", type=bool, default=False, help="Flag to whether preprocess data or not")
 parser.add_argument("--store_preprocess", type=bool, default=False, help="Flag whether to store preprocess data or not")
 args = parser.parse_args()
-
+df[dataset_details["label_encode_on"]] = df[dataset_details["label_encode_on"]].str.lower()
 # Preprocessing functions
 if args.preprocess:
     # Labelling nominal / ordinal variables
@@ -140,10 +143,10 @@ if args.preprocess:
     null_validation = df.isnull().sum()
     print(type(null_validation), null_validation)
 
-
 # Printing new data
 print(df.columns)
 print(df.head())
+print(df["type"].unique())
 
 
 if args.store_preprocess:
